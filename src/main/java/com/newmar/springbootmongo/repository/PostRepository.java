da67983a588b4679a1e725253d483130ad759659
NewmarLucas <newmarlucas@gmail.com>
2023-05-07 17:03:07 -0400

    simple search with query methods

--- src/main/java/com/newmar/springbootmongo/repository/PostRepository.java
+++ src/main/java/com/newmar/springbootmongo/repository/PostRepository.java
@@ -1,9 +1,12 @@
 package com.newmar.springbootmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.newmar.springbootmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
