530a58a46f5e0465246274241747a5081fb19fa4
NewmarLucas <newmarlucas@gmail.com>
2023-05-07 09:17:23 -0400

    removing user

--- src/main/java/com/newmar/springbootmongo/services/UserService.java
+++ src/main/java/com/newmar/springbootmongo/services/UserService.java
@@ -30,6 +30,14 @@ public class UserService {
 		return repo.insert(obj);
 	}
 	
+	public void delete(String id) {
+		Optional<User> user = repo.findById(id);
+		if (user.isEmpty()) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		repo.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
