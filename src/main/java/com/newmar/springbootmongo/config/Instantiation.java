affc233c070ea9c9ed49c15c9113159eb7bcf84e
NewmarLucas <newmarlucas@gmail.com>
2023-05-07 09:58:34 -0400

    saving author data using DTO

--- src/main/java/com/newmar/springbootmongo/config/Instantiation.java
+++ src/main/java/com/newmar/springbootmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.newmar.springbootmongo.domain.Post;
 import com.newmar.springbootmongo.domain.User;
+import com.newmar.springbootmongo.dto.AuthorDTO;
 import com.newmar.springbootmongo.repository.PostRepository;
 import com.newmar.springbootmongo.repository.UserRepository;
 
@@ -37,9 +38,8 @@ public class Instantiation implements CommandLineRunner {
 
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
-				maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
