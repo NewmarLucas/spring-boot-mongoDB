f371150fb18558e067cccd4986a591233f954158
NewmarLucas <newmarlucas@gmail.com>
2023-05-07 17:31:29 -0400

    search with multiple params

--- src/main/java/com/newmar/springbootmongo/repository/PostRepository.java
+++ src/main/java/com/newmar/springbootmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.newmar.springbootmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -10,7 +11,10 @@ import com.newmar.springbootmongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String> {
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
-	
+
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
+
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: {$lte: ?2} }, { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
