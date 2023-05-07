c595c46af98f619b909ec0f6ca56884c079ca0a3
NewmarLucas <newmarlucas@gmail.com>
2023-05-07 10:19:49 -0400

    adding comment in the posts

--- src/main/java/com/newmar/springbootmongo/domain/Post.java
+++ src/main/java/com/newmar/springbootmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.newmar.springbootmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.newmar.springbootmongo.dto.AuthorDTO;
+import com.newmar.springbootmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,6 +22,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+
+	private List<CommentDTO> commets = new ArrayList<>();
 
 	public Post() {
 	}
@@ -70,6 +75,14 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getCommets() {
+		return commets;
+	}
+
+	public void setCommets(List<CommentDTO> commets) {
+		this.commets = commets;
 	}
 
 	@Override
