cc0b28bd33d59dcc0c7c9c820f3209c62a6432eb
NewmarLucas <newmarlucas@gmail.com>
2023-05-06 21:25:58 -0400

    create project

---
+++ src/test/java/com/newmar/springbootmongo/SpringbootmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newmar.springbootmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
