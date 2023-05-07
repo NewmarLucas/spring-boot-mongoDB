b555c59dbd80d4564db83dff097554c2080df5e9
NewmarLucas <newmarlucas@gmail.com>
2023-05-07 17:08:14 -0400

    search with @Query

--- src/main/java/com/newmar/springbootmongo/services/PostService.java
+++ src/main/java/com/newmar/springbootmongo/services/PostService.java
@@ -15,13 +15,14 @@ public class PostService {
 
 	@Autowired
 	private PostRepository repo;
-	
+
 	public Post findById(String id) {
 		Optional<Post> post = repo.findById(id);
 		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public List<Post> findByTitle(String text) {
-		return repo.findByTitleContainingIgnoreCase(text);
+//		return repo.findByTitleContainingIgnoreCase(text);
+		return repo.searchTitle(text);
 	}
 }
